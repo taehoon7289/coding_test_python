fdbecc30698cdb15c88f2af99442cc3de4cfd37c
taehoon7289 <taehoon7289@gmail.com>
2023-01-25 10:36:27 +0900

    feat: 섬_연결하기.py
    - 크루스칼 알고리즘에 대해 학습함
    - 최소신장트리는 크루스칼 알고리즘으로 해결가능하며, 부모노드는 union, find 알고리즘으로 부모노드를 찾고, 지정할수있다.

---
+++ programers/섬_연결하기.py
@@ -0,0 +1,31 @@
+def solution(n, costs):
+    def find_parent(parent, n):
+        if parent[n] == n:
+            return parent[n]
+        return find_parent(parent, parent[n])
+
+    def union_parent(parent, a, b):
+        a = find_parent(parent, a)
+        b = find_parent(parent, b)
+        # 최대한 부모노드가 적은숫자를 가진걸로 통일
+        parent[max(a, b)] = min(a, b)
+
+    answer = 0
+    costs.sort(key=lambda x: x[2])  # 비용 기준으로 오름차순 정렬
+    parent = [i for i in range(n)]
+
+    cnt = 0
+    for node_a, node_b, cost in costs:
+        if find_parent(parent, node_a) != find_parent(parent, node_b):
+            union_parent(parent, node_a, node_b)
+            answer += cost
+            cnt += 1
+        if cnt >= n - 1:
+            break
+
+    return answer
+
+
+if __name__ == '__main__':
+    print(solution(4, [[0, 1, 1], [0, 2, 2], [1, 2, 5], [1, 3, 1], [2, 3, 8]]))
+    print(solution(5, [[0, 1, 5], [1, 2, 3], [2, 3, 3], [3, 1, 2], [3, 0, 4], [2, 4, 6], [4, 0, 7]]))
