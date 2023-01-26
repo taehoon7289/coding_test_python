15319ac57d5da60876c893f9199c1289f26acd88
taehoon7289 <taehoon7289@gmail.com>
2023-01-26 11:44:07 +0900

    feat: 정수_삼각형.py
    - 문제 흐름대로 위에서 아래로 내려가기보단 밑에서 올라오면서 합에 대한 최대값만 남기고 올라오는 형태가 이해하기 쉬웠음.

---
+++ programers/정수_삼각형.py
@@ -0,0 +1,39 @@
+def solution(triangle):
+    answer = 0
+    n = len(triangle)
+    visited = [[0] * n for n in range(n, 0, -1)]
+    for i in range(len(triangle[n - 1])):
+        visited[0][i] = triangle[n - 1][i]
+    for i in range(1, n):
+        # i 는 층수
+        for j in range(len(visited[i])):
+            # j는 각 층수의 인덱스
+            visited[i][j] = max(sum([visited[i - 1][j], triangle[n - i - 1][j]]),
+                                sum([visited[i - 1][j + 1], triangle[n - i - 1][j]]))
+    answer = visited[n - 1][0]
+    return answer
+
+
+def solution2(triangle):
+    answer = 0
+    n = len(triangle)
+    visited = [[0] * n for n in range(1, n + 1)]
+    for i in range(len(triangle[n - 1])):
+        visited[n - 1][i] = triangle[n - 1][i]
+
+    for i in range(n - 2, -1, -1):
+        # i 는 층수
+        for j in range(len(visited[i])):
+            # j는 각 층수의 인덱스
+            visited[i][j] = max(sum([visited[i + 1][j], triangle[i][j]]),
+                                sum([visited[i + 1][j + 1], triangle[i][j]]))
+
+    answer = visited[0][0]
+    return answer
+
+
+if __name__ == '__main__':
+    # 처음에 생각한 역 삼각형 형태로
+    print(solution([[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]))
+    # 삼각형 형태에서 인덱스만 뒤에서 부터 시작
+    print(solution2([[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]))
