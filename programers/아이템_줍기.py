04c52352332bd255899d540d70f46705ba5e9499
taehoon7289 <taehoon7289@gmail.com>
2023-02-04 17:27:05 +0900

    feat: 아이템_줍기.py
    - 저번에 풀었던건데 다시 풀때 시간내에 못품
    - 핵심은 직사각형들이 겹치거나 있을경우 외부 테두리만 어떻게 구하는지에 대한 방법
    - 저번방법은 set을 써서 구함. 이번엔 직사각형의 각각 테두리 표시하고 다시 반복해서 확인할때 내 내부에 표시가 되있다면 제거 하는 방법 사용
    - set을 사용해서 구하는게 빠를듯

--- programers/아이템_줍기.py
+++ programers/아이템_줍기.py
@@ -1,6 +1,3 @@
-from collections import deque
-
-
 def solution(rectangle, characterX, characterY, itemX, itemY):
     answer = 0
     p_set = set()
@@ -48,9 +45,72 @@ def solution(rectangle, characterX, characterY, itemX, itemY):
     return answer
 
 
+from collections import deque
+from pprint import pprint
+
+
+def solution2(rectangle, characterX, characterY, itemX, itemY):
+    answer = float('inf')
+    max_col, max_row = 0, 0
+    rectangle = list(map(lambda s: [s[0] * 2, s[1] * 2, s[2] * 2, s[3] * 2], rectangle))
+    for rec in rectangle:
+        col1, row1, col2, row2 = rec
+        max_col, max_row = max(max_col, col2 + 2), max(max_row, row2 + 2)
+    arr = [[0] * (max_col) for _ in range(max_row)]
+
+    for rec in rectangle:
+        col1, row1, col2, row2 = rec
+        for row in range(len(arr)):
+            for col in range(len(arr[row])):
+                if row1 <= row <= row2:
+                    if col == col1:
+                        arr[row][col] = 1
+                    elif col == col2:
+                        arr[row][col] = 1
+                if col1 <= col <= col2:
+                    if row == row1:
+                        arr[row][col] = 1
+                    elif row == row2:
+                        arr[row][col] = 1
+    for rec in rectangle:
+        col1, row1, col2, row2 = rec
+        for row in range(len(arr)):
+            for col in range(len(arr[row])):
+                if row1 < row < row2 and col1 < col < col2:
+                    arr[row][col] = 0
+
+    # pprint(arr)
+    cases = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+
+    q = deque()
+    # col, row 순
+    q.append([characterX * 2, characterY * 2, 0])
+    while q:
+        n_col, n_row, cnt = q.popleft()
+        # print(n_col, n_row, cnt)
+        if n_col == itemX * 2 and n_row == itemY * 2:
+            answer = cnt
+            break
+        else:
+            for next_r, next_c in cases:
+                m_r, m_c = next_r + n_row, next_c + n_col
+                if 0 <= m_r < max_row and 0 <= m_c < max_col:
+                    if arr[m_r][m_c] == 1:
+                        arr[m_r][m_c] += 1
+                        q.append([m_c, m_r, cnt + 1])
+
+    return answer // 2
+
+
 if __name__ == '__main__':
-    print(solution([[1, 1, 7, 4], [3, 2, 5, 5], [4, 3, 6, 9], [2, 6, 8, 8]], 1, 3, 7, 8))
-    print(solution([[1, 1, 8, 4], [2, 2, 4, 9], [3, 6, 9, 8], [6, 3, 7, 7]], 9, 7, 6, 1))
-    print(solution([[1, 1, 5, 7]], 1, 1, 4, 7))
-    print(solution([[2, 1, 7, 5], [6, 4, 10, 10]], 3, 1, 7, 10))
-    print(solution([[2, 2, 5, 5], [1, 3, 6, 4], [3, 1, 4, 6]], 1, 4, 6, 3))
+    # print(solution([[1, 1, 7, 4], [3, 2, 5, 5], [4, 3, 6, 9], [2, 6, 8, 8]], 1, 3, 7, 8))
+    # print(solution([[1, 1, 8, 4], [2, 2, 4, 9], [3, 6, 9, 8], [6, 3, 7, 7]], 9, 7, 6, 1))
+    # print(solution([[1, 1, 5, 7]], 1, 1, 4, 7))
+    # print(solution([[2, 1, 7, 5], [6, 4, 10, 10]], 3, 1, 7, 10))
+    # print(solution([[2, 2, 5, 5], [1, 3, 6, 4], [3, 1, 4, 6]], 1, 4, 6, 3))
+
+    print(solution2([[1, 1, 7, 4], [3, 2, 5, 5], [4, 3, 6, 9], [2, 6, 8, 8]], 1, 3, 7, 8))
+    print(solution2([[1, 1, 8, 4], [2, 2, 4, 9], [3, 6, 9, 8], [6, 3, 7, 7]], 9, 7, 6, 1))
+    print(solution2([[1, 1, 5, 7]], 1, 1, 4, 7))
+    print(solution2([[2, 1, 7, 5], [6, 4, 10, 10]], 3, 1, 7, 10))
+    print(solution2([[2, 2, 5, 5], [1, 3, 6, 4], [3, 1, 4, 6]], 1, 4, 6, 3))
