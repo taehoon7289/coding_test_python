fd8d34b23ca7771d69e5c573c5f78724c79e9188
taehoon7289 <taehoon7289@gmail.com>
2023-02-05 19:35:13 +0900

    feat: 입국심사.py
    - 다시 이진탐색 문제 출제 되서 풀어봄.
    - 하나하나 따져보면서 진행함.

--- programers/입국심사.py
+++ programers/입국심사.py
@@ -20,6 +20,26 @@ def solution(n, times):
 
     return answer
 
+def solution2(n, times):
+    # 걸리는 시간의 최소값
+    answer = float('inf')
+    # n명이 심사받는데 가장 오래걸리는 시간
+    start, end = 0, max(times) * n
+    while start <= end:
+        # 걸리는 시간 가운데 값
+        mid = (start + end) // 2
+        # 이 시간동안 심사 받을수 있는 사람수 구하기
+        cnt = 0
+        for t in times:
+            cnt += mid // t
+        # cnt 값이 n 보다 작은 경우는 필요없고, 충족하는데 최소값을 찾을꺼라서
+        if cnt >= n:
+            end = mid - 1
+            answer = min(answer, mid)
+        else:
+            start = mid + 1
+    return answer
 
 if __name__ == '__main__':
     print(solution(6, [7, 10]))
+    print(solution2(6, [7, 10]))
